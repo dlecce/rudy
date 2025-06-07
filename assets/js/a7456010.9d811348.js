3ff9b883c9527435c7054827d8564cdca213bf21
Daniele Lecce <daniele.lecce.hoverture@skytv.it>
2025-06-08 00:49:47 +0200

    Deploy website - based on e67bc363ae55ec0294ca00ad1a5d884f04ee2b2b

---
+++ assets/js/a7456010.9d811348.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrudy=self.webpackChunkrudy||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- src/components/sections/Calendario.tsx
+++
@@ -1,8 +0,0 @@
-export default function CalendarioSection() {
-    return (
-        <section id="calendario">
-            <h2>📅 Calendario</h2>
-            <li>Per semplificare l'organizzazione, ho creato un calendario dove sono segnati i turni di ciascuno. → <a href="https://calendar.google.com/calendar/embed?src=db1b76b75eecf0fab3c35e7cba5ff940a3a6733fa780813e8d6b867a95361a7d%40group.calendar.google.com&ctz=Europe%2FRome">CALENDARIO</a>.</li>
-        </section>
-    );
-}
